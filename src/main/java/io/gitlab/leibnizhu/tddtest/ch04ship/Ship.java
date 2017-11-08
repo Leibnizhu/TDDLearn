fc6910a6fd3e53df9e6e2f2054bb6d7259f38303
Leibniz.Hu <Leibnizhu@gmail.com>
2017-11-08 13:16:15 +0800

    完成Ship基本功能及测试

--- src/main/java/io/gitlab/leibnizhu/tddtest/ch04ship/Ship.java
+++ src/main/java/io/gitlab/leibnizhu/tddtest/ch04ship/Ship.java
@@ -1,5 +1,61 @@
 package io.gitlab.leibnizhu.tddtest.ch04ship;
 
 public class Ship {
+    private final Planet planet;
+    private final Location location;
 
+    public Ship(Location location, Planet planet) {
+        this.location = location;
+        this.planet = planet;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public Planet getPlanet() {
+        return planet;
+    }
+
+    public boolean moveForward() {
+        return location.forward(planet.getMax());
+    }
+
+    public boolean moveBackward() {
+        return location.backward(planet.getMax());
+    }
+
+    public Ship turnLeft() {
+        location.turnLeft();
+        return this;
+    }
+
+    public Ship turnRight() {
+        location.turnRight();
+        return this;
+    }
+
+    public void receiveCommand(String command) {
+        switch (command) {
+            case "F":
+                moveForward();
+                break;
+            case "B":
+                moveBackward();
+                break;
+            case "L":
+                turnLeft();
+                break;
+            case "R":
+                turnRight();
+                break;
+            default:
+        }
+    }
+
+    public void receiveCommands(String commands) {
+        for(String command : commands.split("")){
+            receiveCommand(command);
+        }
+    }
 }
--- src/test/java/io/gitlab/leibnizhu/tddtest/ch04ship/ShipSpec.java
+++ src/test/java/io/gitlab/leibnizhu/tddtest/ch04ship/ShipSpec.java
@@ -1,8 +1,146 @@
 package io.gitlab.leibnizhu.tddtest.ch04ship;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
 
 @Test
 public class ShipSpec {
+    private Planet planet;
+    private Location location;
+    private Ship ship;
 
+    @BeforeMethod //等价于JUint的@Before
+    public void beforeTest() {
+        this.location = new Location(new Point(21, 13), Direction.NORTH);
+        this.planet = new Planet(new Point(50, 50));
+        this.ship = new Ship(this.location, this.planet);
+    }
+
+    public void whenInstantiatedTHenLocationIsSet() {
+        assertEquals(ship.getLocation(), location);
+    }
+
+    public void whenInstantiatedTHenPlanetIsSet() {
+        assertEquals(ship.getPlanet(), planet);
+    }
+
+    /**
+     * 前进
+     */
+    public void whenMoveForwardThenForward() {
+        Location expected = location.copy();
+        expected.forward();
+        ship.moveForward();
+        assertEquals(ship.getLocation(), expected);
+    }
+
+    /**
+     * 后退
+     */
+    public void whenMoveBackwardThenBackward() {
+        Location expected = location.copy();
+        expected.backward();
+        ship.moveBackward();
+        assertEquals(ship.getLocation(), expected);
+    }
+
+    /**
+     * 左转
+     */
+    public void whenTurnLeftThenTurnLeft() {
+        Location expected = location.copy();
+        expected.turnLeft();
+        ship.turnLeft();
+        assertEquals(ship.getLocation(), expected);
+    }
+
+    /**
+     * 右转
+     */
+    public void whenTurnRightThenTurnRight() {
+        Location expected = location.copy();
+        expected.turnRight();
+        ship.turnRight();
+        assertEquals(ship.getLocation(), expected);
+    }
+
+    /**
+     * 接受F命令
+     */
+    public void whenReceiveCommandFThenForward() {
+        Location expected = location.copy();
+        expected.forward();
+        ship.receiveCommand("F");
+        assertEquals(ship.getLocation(), expected);
+    }
+
+    /**
+     * 接受B命令
+     */
+    public void whenReceiveCommandBThenBackward() {
+        Location expected = location.copy();
+        expected.backward();
+        ship.receiveCommand("B");
+        assertEquals(ship.getLocation(), expected);
+    }
+
+    /**
+     * 接受L命令
+     */
+    public void whenReceiveCommandLThenTurnLeft() {
+        Location expected = location.copy();
+        expected.turnLeft();
+        ship.receiveCommand("L");
+        assertEquals(ship.getLocation(), expected);
+    }
+
+    /**
+     * 接受R命令
+     */
+    public void whenReceiveCommandRThenTurnRight() {
+        Location expected = location.copy();
+        expected.turnRight();
+        ship.receiveCommand("R");
+        assertEquals(ship.getLocation(), expected);
+    }
+
+    /**
+     * 执行组合命令
+     */
+    public void whenReceiveCommands() {
+        Location expected = location.copy();
+        expected.forward();
+        expected.backward();
+        expected.turnLeft();
+        ship.receiveCommands("FBL");
+        assertEquals(ship.getLocation(), expected);
+
+        expected.turnLeft();
+        expected.forward();
+        ship.receiveCommands("LF");
+        assertEquals(ship.getLocation(), expected);
+    }
+
+    /**
+     * 前进超越东边边界
+     */
+    public void whenForwardOverpassEastBoundaryThenGoWest(){
+        location.setDirection(Direction.EAST);
+        location.getPoint().setX(planet.getMax().getX());
+        ship.receiveCommand("F");
+        assertEquals(location.getX(), 1);
+    }
+
+    /**
+     * 倒退超越东边边界
+     */
+    public void whenBackwardOverpassEastBoundaryThenGoWest(){
+        location.setDirection(Direction.EAST);
+        location.getPoint().setX(planet.getMax().getX());
+        ship.turnRight().turnRight();
+        ship.receiveCommand("B");
+        assertEquals(location.getX(), 1);
+    }
 }
